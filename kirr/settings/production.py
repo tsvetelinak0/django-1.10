a7cb10a661af7fbc4f9cbd41a901f90b97eb0273
Tsvetelina Koleva <tsekoleva@gmail.com>
2021-01-27 18:18:18 +0000

    Set DEBUG value

--- kirr/settings/production.py
+++ kirr/settings/production.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
 ALLOWED_HOSTS = ['hashedurl.herokuapp.com', 'hashedurl.com', 'www.hashedurl.com']
 
